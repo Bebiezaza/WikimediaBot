3d4b3354cf5c0f8946653b6fab05733264b79b0f
Bebiezaza <Apipunnakul@gmail.com>
2023-01-09 23:40:47 +0700

    check server response from http and https

--- mwn/support/checkServerResponse.js
+++ mwn/support/checkServerResponse.js
@@ -1,10 +1,17 @@
 const http = require("http");
+const https = require("https");
 const fs = require('fs');
 
 const checker = JSON.parse(fs.readFileSync("./output/gazette.json"));
 
 for (let x in checker) {
-    http.get(checker[x].link, (res) => {
-        fs.appendFileSync("./output/checkLink.txt", `[${res.statusCode}] ${checker[x].fileName}\n`);
-    });
+    if (checker[x].link.indexOf("https") !== -1) {
+        https.get(checker[x].link, (res) => {
+            fs.appendFileSync("./output/checkLink.txt", `[${res.statusCode}] ${checker[x].fileName}\n`);
+        });
+    } else {
+        http.get(checker[x].link, (res) => {
+            fs.appendFileSync("./output/checkLink.txt", `[${res.statusCode}] ${checker[x].fileName}\n`);
+        });
+    }
 }
