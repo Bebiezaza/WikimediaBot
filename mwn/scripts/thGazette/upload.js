26fdceb5f0220f95e83590916496ebbe95dfc6c8
Bebiezaza <Apipunnakul@gmail.com>
2023-01-12 01:05:42 +0700

    edit after first test run

--- mwn/scripts/thGazette/upload.js
+++ mwn/scripts/thGazette/upload.js
@@ -4,8 +4,8 @@ const { commonsBot, thwsBot } = require("../../core/bot");
 
 const input = JSON.parse(fs.readFileSync("./input/gazette.json"));
 
-let counter = 0;
-const max = input.length - 1;
+let counter = 7; // 0
+const max = 29; //input.length - 1;
 mwn.log(`[I] There are ${max - counter + 1} upload and index creation operations to do`);
 
 const mainClock = setInterval(function () {
@@ -20,15 +20,19 @@ const mainClock = setInterval(function () {
 
 async function main(x) {
     mwn.log(`[I] ${x + 1} : uploading "${input[x].fileName}" to commons`);
-    await commonsBot.uploadFromUrl(input[x].link, input[x].fileName, input[x].commons).then(function(resolve) {
+    await commonsBot.uploadFromUrl(input[x].link, input[x].fileName, input[x].commons, { comment: "Uploading documents from Royal Thai Government Gazette ([[User:BebiezazaBot/Task/1.1|Task 1 (1.1)]] test run)"/*, bot: true*/ }).then(function(resolve) {
         if (resolve.result === 'Success') mwn.log(`[S] ${x + 1} : Uploaded!`);
         else console.log(resolve);
     }).catch(function(error) {
+        if (String(error).indexOf("http-bad-status") !== -1) {
+            mwn.log(`[W] ${x + 1} : http-bad-status, retrying`);
+            return main(x);
+        }
         mwn.log(`[E] ${x + 1} : ${error}`);
     });
 
     mwn.log(`[I] ${x + 1} : Creating index page for "${input[x].fileName}"`);
-    await thwsBot.save(`ดัชนี:${input[x].fileName}`, input[x].index, `สร้าง[[ดัชนี:${input[x].fileName}]] (Task N)`, { watchlist: 'unwatch' }).then(function(resolve) {
+    await thwsBot.save(`ดัชนี:${input[x].fileName}`, input[x].index, `จัดทำหน้าดัชนีสำหรับกลุ่มคำสั่งคสช. ที่ประกาศในราชกิจจานุเบกษาที่บอตได้อัปโหลดไว้ที่คอมมอนส์ ([[ผู้ใช้:BebiezazaBot/Task/5.1|Task 5.1]])`, { watchlist: 'unwatch', bot: true }).then(function(resolve) {
         if (resolve.result === 'Success') mwn.log(`[S] ${x + 1} : Index created!`);
         else console.log(resolve);
     }).catch(function(error) {
